16b05d3572f430cf88350e03f0a250eef07975bb
Jaeseung Park <jaeseung.p@gmail.com>
2019-01-29 15:49:12 +0900

    Add files via upload

---
+++ research/object_detection/asb_test.py
@@ -0,0 +1,54 @@
+from distutils.version import StrictVersion
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+
+# This is needed since the notebook is stored in the object_detection folder.
+sys.path.append("..")
+from object_detection.utils import ops as utils_ops
+
+if StrictVersion(tf.__version__) < StrictVersion('1.9.0'):
+  raise ImportError('Please upgrade your TensorFlow installation to v1.9.* or later!')
+
+# This is needed to display the images.
+%matplotlib inline
+
+from utils import label_map_util
+
+from utils import visualization_utils as vis_util
+
+
+# What model to download.
+MODEL_NAME = 'faster_rcnn_resnet50_coco_2018_01_28'
+MODEL_FILE = MODEL_NAME + '.tar.gz'
+DOWNLOAD_BASE = '/home/sdsra/Documents/ObjectDetection/models/research/object_detection/asb_export_dir/'
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = os.path.join('data', 'asb_label_map_with_label.pbtxt')
+
+NUM_CLASSES = 90
+
+
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+
+
+
--- research/object_detection/export_tflite_ssd_graph_lib.py
+++ research/object_detection/export_tflite_ssd_graph_lib.py
@@ -59,15 +59,9 @@ def get_const_center_size_encoded_anchors(anchors):
   return encoded_anchors
 
 
-def append_postprocessing_op(frozen_graph_def,
-                             max_detections,
-                             max_classes_per_detection,
-                             nms_score_threshold,
-                             nms_iou_threshold,
-                             num_classes,
-                             scale_values,
-                             detections_per_class=100,
-                             use_regular_nms=False):
+def append_postprocessing_op(frozen_graph_def, max_detections,
+                             max_classes_per_detection, nms_score_threshold,
+                             nms_iou_threshold, num_classes, scale_values):
   """Appends postprocessing custom op.
 
   Args:
@@ -83,10 +77,6 @@ def append_postprocessing_op(frozen_graph_def,
     scale_values: scale values is a dict with following key-value pairs
       {y_scale: 10, x_scale: 10, h_scale: 5, w_scale: 5} that are used in decode
       centersize boxes
-    detections_per_class: In regular NonMaxSuppression, number of anchors used
-    for NonMaxSuppression per class
-    use_regular_nms: Flag to set postprocessing op to use Regular NMS instead
-      of Fast NMS.
 
   Returns:
     transformed_graph_def: Frozen GraphDef with postprocessing custom op
@@ -131,10 +121,6 @@ def append_postprocessing_op(frozen_graph_def,
       attr_value_pb2.AttrValue(f=scale_values['h_scale'].pop()))
   new_output.attr['w_scale'].CopyFrom(
       attr_value_pb2.AttrValue(f=scale_values['w_scale'].pop()))
-  new_output.attr['detections_per_class'].CopyFrom(
-      attr_value_pb2.AttrValue(i=detections_per_class))
-  new_output.attr['use_regular_nms'].CopyFrom(
-      attr_value_pb2.AttrValue(b=use_regular_nms))
 
   new_output.input.extend(
       ['raw_outputs/box_encodings', 'raw_outputs/class_predictions', 'anchors'])
@@ -147,14 +133,9 @@ def append_postprocessing_op(frozen_graph_def,
   return transformed_graph_def
 
 
-def export_tflite_graph(pipeline_config,
-                        trained_checkpoint_prefix,
-                        output_dir,
-                        add_postprocessing_op,
-                        max_detections,
-                        max_classes_per_detection,
-                        detections_per_class=100,
-                        use_regular_nms=False):
+def export_tflite_graph(pipeline_config, trained_checkpoint_prefix, output_dir,
+                        add_postprocessing_op, max_detections,
+                        max_classes_per_detection):
   """Exports a tflite compatible graph and anchors for ssd detection model.
 
   Anchors are written to a tensor and tflite compatible graph
@@ -170,10 +151,7 @@ def export_tflite_graph(pipeline_config,
       TFLite_Detection_PostProcess custom op
     max_detections: Maximum number of detections (boxes) to show
     max_classes_per_detection: Number of classes to display per detection
-    detections_per_class: In regular NonMaxSuppression, number of anchors used
-    for NonMaxSuppression per class
-    use_regular_nms: Flag to set postprocessing op to use Regular NMS instead
-      of Fast NMS.
+
 
   Raises:
     ValueError: if the pipeline config contains models other than ssd or uses an
@@ -256,15 +234,11 @@ def export_tflite_graph(pipeline_config,
   tf.train.get_or_create_global_step()
 
   # graph rewriter
-  is_quantized = pipeline_config.HasField('graph_rewriter')
-  if is_quantized:
+  if pipeline_config.HasField('graph_rewriter'):
     graph_rewriter_config = pipeline_config.graph_rewriter
     graph_rewriter_fn = graph_rewriter_builder.build(
         graph_rewriter_config, is_training=False)
     graph_rewriter_fn()
-
-  if pipeline_config.model.ssd.feature_extractor.HasField('fpn'):
-    exporter.rewrite_nn_resize_op(is_quantized)
 
   # freeze the graph
   saver_kwargs = {}
@@ -298,8 +272,7 @@ def export_tflite_graph(pipeline_config,
   if add_postprocessing_op:
     transformed_graph_def = append_postprocessing_op(
         frozen_graph_def, max_detections, max_classes_per_detection,
-        nms_score_threshold, nms_iou_threshold, num_classes, scale_values,
-        detections_per_class, use_regular_nms)
+        nms_score_threshold, nms_iou_threshold, num_classes, scale_values)
   else:
     # Return frozen without adding post-processing custom op
     transformed_graph_def = frozen_graph_def
